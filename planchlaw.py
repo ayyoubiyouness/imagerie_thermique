62f6dae1ac9d79904519cc65379a3ad1b419e384
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2023-05-29 20:37:41 +0100

    Update planchlaw.py
    code commenté

--- planchlaw.py
+++ planchlaw.py
@@ -1,24 +1,30 @@
-# ce code permet de donner la loi de planck pour différents températures. la longeur d'onde est entre 300 nm et 1020 nm
+# ce code permet de représenter la loi de planck pour différents températures. la longeur d'onde est entre 300 nm et 1020 nm
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+# on définit les constantes utilisées dans la loi de planck
 h = 6.626e-34
 c = 3.0e+8
 k = 1.38e-23
+# cette fonction prend en entrée une liste de longueur d'onde et une température T et retourne les valeurs 
+# de l'énergie spectrale pour chaque longueur d'onde
 def planck(wav, T):
     a = 2.0*h*c**2
     b = h*c/(wav*k*T)
     intensity = a/ ( (wav**5) * (np.exp(b) - 1.0) )
     return intensity
+# on définit la liste des longueurs d'onde, ici entre 300 nm et 1020 nm
 wavelengthsPlan = np.arange(300e-9, 1020e-9, 10e-9)
-intensity3000 = planck(wavelengthsPlan, 3000.)
-intensity2800 = planck(wavelengthsPlan, 2800.)
-intensity2600 = planck(wavelengthsPlan, 2600.)
-intensity2400 = planck(wavelengthsPlan, 2400.)
-intensity2200 = planck(wavelengthsPlan, 2200.)
-intensity2000 = planck(wavelengthsPlan, 2000.)
-intensity1800 = planck(wavelengthsPlan, 1800.)
-plt.figure(1)
+# on calcule les énergies spectrales pour différentes températures
+intensity3000 = planck(wavelengthsPlan, 3000.) # T = 3000 K
+intensity2800 = planck(wavelengthsPlan, 2800.) # T = 2800 K
+intensity2600 = planck(wavelengthsPlan, 2600.) # T = 2600 K
+intensity2400 = planck(wavelengthsPlan, 2400.) # T = 2400 K
+intensity2200 = planck(wavelengthsPlan, 2200.) # T = 2200 K
+intensity2000 = planck(wavelengthsPlan, 2000.) # T = 2000 K
+intensity1800 = planck(wavelengthsPlan, 1800.) # T = 1800 K
+
+# Ensuite, on trace les courbes des énergies spectrales dans la meme courbe
 plt.plot(wavelengthsPlan, intensity3000, 'r-', label="Température 3000K")
 plt.plot(wavelengthsPlan, intensity2800, 'b-', label="Température 2800K")
 plt.plot(wavelengthsPlan, intensity2600, 'y-', label="Température 2600K")
